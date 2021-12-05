1e3372e097cc665ab55b56423a0ad0649dcdd90e
Maxon <maxdc7613@gmail.com>
2021-12-05 18:42:13 +0100

    The mod can now intercept chat messages showed by the gui (not the actual purpose of the mod but meant to understand how mixins works

--- src/main/java/me/mwon/discordminecraftlinker/mixin/InterceptChatMessages.java
+++ src/main/java/me/mwon/discordminecraftlinker/mixin/InterceptChatMessages.java
@@ -1,16 +1,17 @@
 package me.mwon.discordminecraftlinker.mixin;
 
 import me.mwon.discordminecraftlinker.DiscordMinecraftLinker;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(ChatHud.class)
 public class InterceptChatMessages {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		DiscordMinecraftLinker.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+    @Inject(at = @At("HEAD"), method = "addMessage(Lnet/minecraft/text/Text;)V")
+        private void addMessage(Text message, CallbackInfo info) {
+            DiscordMinecraftLinker.LOGGER.info("Message received : " + message.getString());
+        }
 }
